0063635873c6387e68450bbdee9f288acf951a5f
Fan Luo <43394670+TheKinginTheNorth-BrandonLuo@users.noreply.github.com>
2020-11-17 23:55:33 -0500

    Update Winnow.java
    This is the process of winnowing algorithm.

--- Winnow.java
+++ Winnow.java
@@ -1,84 +1,37 @@
-/* Copyright (C) 2002 Univ. of Massachusetts Amherst, Computer Science Dept.
-   This file is part of "MALLET" (MAchine Learning for LanguagE Toolkit).
-   http://www.cs.umass.edu/~mccallum/mallet
-   This software is provided under the terms of the Common Public License,
-   version 1.0, as published by http://www.opensource.org.  For further
-   information, see the file `LICENSE' included with this distribution. */
-
-
-
-/** 
-   @author Aron Culotta <a href="mailto:culotta@cs.umass.edu">culotta@cs.umass.edu</a>
- */
-
-package cc.mallet.classify;
-
-import cc.mallet.pipe.Pipe;
-import cc.mallet.types.FeatureVector;
-import cc.mallet.types.Instance;
-import cc.mallet.types.LabelVector;
-
-/** 
- * Classification methods of Winnow2 algorithm.
- * @see WinnowTrainer
- */
-public class Winnow extends Classifier{
-	/**
-	 *array of weights, one for each feature, initialized to 1
-	 */
-	double [][] weights;
-	/**
-	 *threshold for sum of wi*xi in formulating guess 
-	 */
-	double theta;
-	
-	/**
-	 * Passes along data pipe and weights from 
-	 * {@link #WinnowTrainer WinnowTrainer}
-	 * @param dataPipe needed for dictionary, labels, feature vectors, etc
-	 * @param newWeights weights calculated during training phase
-	 * @param theta value used for threshold
-	 * @param idim i dimension of weights array
-	 * @param jdim j dimension of weights array
-	 */
-	public Winnow (Pipe dataPipe,
-								 double [][]newWeights, double theta, 
-								 int idim, int jdim){
-		super (dataPipe);
-		this.theta = theta;
-		this.weights = new double[idim][jdim];
-		for(int i=0; i<idim; i++)
-	    for(int j=0; j<jdim; j++)
-				this.weights[i][j] = newWeights[i][j];
-	}
-	
-	/**
-	 * Classifies an instance using Winnow's weights
-	 * @param instance an instance to be classified
-	 * @return an object containing the classifier's guess
-     */
-	public Classification classify (Instance instance){
-		int numClasses = getLabelAlphabet().size();
-		double[] scores = new double[numClasses];
-		FeatureVector fv = (FeatureVector) instance.getData ();
-		// Make sure the feature vector's feature dictionary matches
-		// what we are expecting from our data pipe (and thus our notion
-		// of feature probabilities.
-		assert (instancePipe == null || fv.getAlphabet () == this.instancePipe.getDataAlphabet ());
-		int fvisize = fv.numLocations();
-		
-		// Set the scores by summing wi*xi
-		for (int fvi = 0; fvi < fvisize; fvi++) {
-			int fi = fv.indexAtLocation (fvi);
-			for (int ci = 0; ci < numClasses; ci++)
-		    scores[ci] += this.weights[ci][fi];
-		}
-		
-		
-		// Create and return a Classification object
-		return new Classification (instance, this,
-															 new LabelVector (getLabelAlphabet(),
-																								scores));
-	}		
+void winnow(int w /*size of window*/ ){   //size of window should less than total number of hash values
+   int *h=new int[w];   //a loop window that holds hash values
+     int min=0;      //records the position of the current minimum in the window
+    for(int i=0;i<w-1;i++){
+        h[i]=next_hash();
+        if(h[i]==-1){       //Executed when the window value is greater than the total number of fingerprints
+             printf("[%d, %d]\n",h[min],pos);
+             delete h;
+             return;
+        }
+    }
+   int r=w-1;       //Iterate through groups of H, adding the hash value of the original sequence to the array in order
+    int pos=min;     //Record the position where the minimum appears in the original sequence
+   while(true){
+          r=(r+1)%w;
+          h[r]=next_hash();   //Add the next hash value
+          if(h[r]==-1)break;  //Pretend end up with -1
+          if(min==r){        //If this is true, the minimum value of the previous window will be removed
+              pos+=w;
+              for(int i=(r-1+w)%w; i!=r; i=(i-1+w)%w){   //Selects the smallest and rightmost hash value in the window
+                  if(h[i]<h[min]){
+                    pos-=(min+w-i)%w;
+                    min=i;
+                }
+            }
+              printf("[%d, %d]\n",h[min],pos);
+         }
+          else{                             //min!=r indicates that the current minimum hash value is still contained in the window
+            if(h[r]<=h[min]){      //We only need to compare the newly added hash value
+                  pos+=(r+w-min)%w;
+                  min=r;
+                  printf("[%d, %d]\n",h[min],pos);
+            }
+          }
+   }
+   delete h;
 }
-
