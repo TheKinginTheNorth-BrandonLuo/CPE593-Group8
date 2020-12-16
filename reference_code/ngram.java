4c0da266cc80c4d10c6728e0adfe305c45f56f5b
Fan Luo <43394670+TheKinginTheNorth-BrandonLuo@users.noreply.github.com>
2020-12-15 23:14:17 -0500

    Update ngram.java

--- reference_code/ngram.java
+++ reference_code/ngram.java
@@ -14,18 +14,14 @@ public static String concat(String[] words, int start, int end) {
     }
 
 public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> unSortedMap) {
-        // System.out.println("Unsorted Map : " + unSortedMap);
 
-        // LinkedHashMap保留插入元素的顺序
     LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
 
-        // 使用 Comparator.reverseOrder() 进行反向排序
     unSortedMap.entrySet()
             .stream()
             .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
             .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
 
-        // System.out.println("Reverse Sorted Map   : " + reverseSortedMap);
 
     return reverseSortedMap;
     }
@@ -34,13 +30,6 @@ public static void main(String[] args) {
     HashMap<String, Integer> count = new HashMap<>();
     String text = "This is just a simple test to test how n gram worked.";
 
-        // 生成n为1~3的N元语法
-//        for (int n = 1; n <= 3; n++) {
-//            for (String ngram : ngrams(n, text)) {
-//                System.out.println(ngram);
-//            }
-//            System.out.println();
-//        }
 
     for (String ngram : ngrams(3, text)) {
         // counting ngram by using HashMap
@@ -52,7 +41,6 @@ public static void main(String[] args) {
         System.out.println(ngram);
     }
 
-        // 按出现次由多到少的顺序打印ngram
     System.out.println("\nCounting Result: ");
     for (Map.Entry<String, Integer> entry : sortByValue(count).entrySet()) {
         System.out.println(entry.getKey() + ": " + entry.getValue());
